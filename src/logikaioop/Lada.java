6c19343103419f4563a83642be5abfe8ff2448ce
fureszb <fureszb@gmail.com>
2024-01-08 10:35:23 +0100

    lada

---
+++ src/logikaioop/Lada.java
@@ -0,0 +1,44 @@
+package logikaioop;
+
+
+public class Lada {
+
+    private String anyag, allitas;
+    private boolean kincs;
+
+    @Override
+    public String toString() {
+        return "Lada{" + "anyag=" + anyag + ", allitas=" + allitas + ", kincs=" + kincs + '}';
+    }
+
+    public Lada(String anyag, String allitas, boolean kincs) {
+        this.anyag = anyag;
+        this.allitas = allitas;
+        this.kincs = kincs;
+    }
+
+    public String getAnyag() {
+        return anyag;
+    }
+
+    public void setAnyag(String anyag) {
+        this.anyag = anyag;
+    }
+
+    public String getAllitas() {
+        return allitas;
+    }
+
+    public void setAllitas(String allitas) {
+        this.allitas = allitas;
+    }
+
+    public boolean isKincs() {
+        return kincs;
+    }
+
+    public void setKincs(boolean kincs) {
+        this.kincs = kincs;
+    }
+
+}
