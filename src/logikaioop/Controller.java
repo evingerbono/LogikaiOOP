13a2a493fb4bd11a35942c06d8fe1f0b03a2a9a2
fureszb <fureszb@gmail.com>
2024-01-08 11:40:33 +0100

    TovabbFEjlesztes

--- src/logikaioop/Controller.java
+++ src/logikaioop/Controller.java
@@ -4,23 +4,42 @@ import java.util.Scanner;
 
 public class Controller {
 
-    public Controller() {
+    private static final Scanner sc = new Scanner(System.in);
+    private Lada[] tomb;
 
+    public Controller() {
+        tomb = new Lada[3];
         System.out.println("Leírás");
-        Lada l1= new Lada("arany", "Az 1.állítás", true);
-        System.out.println(l1.getAllitas());
-        Lada l2= new Lada("ezüst", "A 2.állítás", true);
-        System.out.println(l2.getAllitas());
-        Lada l3= new Lada("bronz", "A 3.állítás", true);
-        System.out.println(l3.getAllitas());
+        tomb[0] = new Lada("arany", "Az 1.állítás", false);
+
+        tomb[1] = new Lada("ezüst", "A 2.állítás", false);
+
+        tomb[2] = new Lada("bronz", "A 3.állítás", true);
+
+        for (Lada lada : tomb) {
+            System.out.println(lada.getAllitas());
+        }
+
         System.out.println("Melyik:");
-        Beker();
-        System.out.println("A válasz jó | rossz");
+        eredemeny();
+       
     }
 
-    public void Beker() {
-        Scanner sc = new Scanner(System.in);
-        String valasz;
-        valasz = sc.next();
+    private void eredemeny() {
+        if(isHelyes(beker())){
+        
+            System.out.println("A válasz jó");
+        }
+        else{
+            System.out.println("A válasz rossz");
+        }
+    }
+
+    public int beker() {
+        return sc.nextInt()-1;
+    }
+
+    public boolean isHelyes(int szam) {
+        return tomb[szam].isKincs();
     }
 }
